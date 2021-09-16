091c35ae6164852587a73d8c27818a04b4ee6b61
nibedita260 <deepi.nibedita260.com>
2021-09-16 12:41:43 +0530

    [Nibedita]Add:PartTime EmployeeWage Feature Implemented

--- EmpWage/EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWage/EmpWageComputation.cs
@@ -6,26 +6,19 @@ namespace EmpWage
 {
     class EmpWageComputation
     {
-        const int IS_PRESENT = 1, WAGE_PER_HOUR = 20, FULL_DAY_HR = 8;
+        const int IS_PRESENT = 1, WAGE_PER_HOUR = 20, FULL_DAY_HR = 8,PART_DAY_HR=4;
         int totalEmpWage=0;
-        public void Attendance()
-        {
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
-        }
+        
         public void EmpWageCal()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
-                totalEmpWage = WAGE_PER_HOUR * FULL_DAY_HR;
+                totalEmpWage = WAGE_PER_HOUR * PART_DAY_HR;
             else
                 totalEmpWage = 0;
             Console.WriteLine("Total emp wage is: " +totalEmpWage);
+
         }
     }
 }
