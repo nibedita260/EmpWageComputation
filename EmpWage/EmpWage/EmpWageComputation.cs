2ed162d24b3c563a134300c8d959312e7bcbe6fb
nibedita260 <deepi.nibedita260.com>
2021-09-16 13:07:04 +0530

    [Nibedita]Add:Switch case EmployeeWage Feature Implemented

--- EmpWage/EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWage/EmpWageComputation.cs
@@ -6,17 +6,31 @@ namespace EmpWage
 {
     class EmpWageComputation
     {
-        const int IS_PRESENT = 1, WAGE_PER_HOUR = 20, FULL_DAY_HR = 8,PART_DAY_HR=4;
-        int totalEmpWage=0;
+        const int IS_PRESENT = 1, WAGE_PER_HOUR = 20, FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULLTIME = 1, IS_PARTTIME = 2;
+        int totalEmpWage = 0, partTimeEmpWage = 0, empHrs = 0;
         
         public void EmpWageCal()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
-                totalEmpWage = WAGE_PER_HOUR * PART_DAY_HR;
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PRESENT) {
+                totalEmpWage = WAGE_PER_HOUR * FULL_DAY_HR;
+            }
             else
+            {
                 totalEmpWage = 0;
+            }
+            switch (empCheck)
+            {
+                case IS_FULLTIME: empHrs += FULL_DAY_HR;
+                    break;
+                case IS_PARTTIME: empHrs += PART_DAY_HR;
+                    break;
+                default:
+                    empHrs += 0;
+                    break;
+            }
+            totalEmpWage = empHrs * WAGE_PER_HOUR;
             Console.WriteLine("Total emp wage is: " +totalEmpWage);
 
         }
