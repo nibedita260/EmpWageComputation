32abfaab6371bf4f1d73f42da64dd79a07eb2450
nibedita260 <deepi.nibedita260.com>
2021-09-17 14:50:00 +0530

    [Nibedita]Add:Add Companies EmpWageComputation Feature Implemented

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -7,8 +7,7 @@ namespace EmpWage
         static void Main(string[] args)
         {
             EmpWageComputation empWageComputation = new EmpWageComputation();
-            Console.WriteLine(empWageComputation.ComputeEmpWage("bridgelabz",20,100,20));
-            Console.WriteLine(empWageComputation.ComputeEmpWage("Google", 100, 100, 20));
+            Console.WriteLine("Added companies in empwage computation method");
         }
     }
 }
