423196711a0d34afece612f542e505843579b225
nibedita260 <deepi.nibedita260.com>
2021-09-17 14:19:09 +0530

    [Nibedita]Add:Till DaysandHrs EmpWageComputation Feature Implemented

--- EmpWage/EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWage/EmpWageComputation.cs
@@ -7,15 +7,15 @@ namespace EmpWage
     class EmpWageComputation
     {
         const int IS_PRESENT = 1, WAGE_PER_HOUR = 20, FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULLTIME = 1, IS_PARTTIME = 2,WORKING_DAY_MONTH=20, TOTAL_WORKING_HRS=100;
-        int totalEmpWage = 0, empHrs = 0, working_days=0, working_hrs=0, dailyEmpWages=0;
+        int totalEmpWage = 0, empHrs = 0, workingDays = 0;
         
         public void EmpWageCal()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            while (working_days < WORKING_DAY_MONTH && working_hrs < TOTAL_WORKING_HRS)
+            while (workingDays < WORKING_DAY_MONTH && empHrs <= TOTAL_WORKING_HRS)
             {
-                working_days = working_days + 1;
+                workingDays = workingDays + 1;
                 switch (empCheck)
                 {
                     case IS_FULLTIME:
@@ -31,9 +31,8 @@ namespace EmpWage
                 Console.WriteLine("emp hrs: " + empHrs);
             }
             //cal wages till workinghrs is 100 and workingdays is 20
-            working_hrs = working_hrs + empHrs;
-            Console.WriteLine("working hrs: " + working_hrs + ",working days:" + working_days);
-            totalEmpWage = working_hrs * WAGE_PER_HOUR;
+            Console.WriteLine("working hrs: " + empHrs + ",working days:" + workingDays);
+            totalEmpWage = empHrs * WAGE_PER_HOUR;
                 Console.WriteLine("Total emp wage is: "+totalEmpWage);
         }
     }
